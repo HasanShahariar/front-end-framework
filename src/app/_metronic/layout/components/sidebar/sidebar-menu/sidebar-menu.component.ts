385bbb3eda15a4b8e3e20a343e324b0a83bcede7
shahariar09 <hasanshahariar550@gmail.com>
2024-07-25 14:41:22 +0600

    initial commit

---
+++ src/app/_metronic/layout/components/sidebar/sidebar-menu/sidebar-menu.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { LayoutService } from '../../../core/layout.service';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-sidebar-menu',
+  templateUrl: './sidebar-menu.component.html',
+  styleUrls: ['./sidebar-menu.component.scss']
+})
+export class SidebarMenuComponent implements OnInit {
+  private authLocalStorageToken = `${environment.appVersion}-${environment.USERDATA_KEY}`;
+  menues: any;
+
+  constructor(
+    private layoutService:LayoutService
+  ) { }
+
+  ngOnInit(): void {
+    this.getMenu();
+  }
+
+  getMenu(){
+    var user = JSON.parse(localStorage.getItem(this.authLocalStorageToken));
+    
+    this.layoutService.getMenuByUser(Number(user.Id)).subscribe(
+      (data)=>{
+        
+        this.menues = data;
+        console.log(this.menues);
+      },
+      (err)=>{
+        console.log(err);
+        
+      }
+    )
+  }
+
+
+
+}
